88a2ae374f95b9476993f6f2a57f497dc916a55a
iris paredes <irisparedesjimenez@gmail.com>
2019-06-30 20:54:26 +0200

    add folder structure and components

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        prueba
+      </header>
+    </div>
+  );
+}
+
+export default App;
